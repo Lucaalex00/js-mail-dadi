ccb79f2f602cd14a59007bff3ea5cba22a398d44
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-02-22 15:04:04 +0100

    add Mail Ex commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log('try')
+/*
+STUFF
+let/const
+if/else if
+numbers
+strings
+array[]
+prompt
+getelementbyID/ querySelector
+HTML ELEMENTS
+*/
+/* console.log('try') */
+let mail_list = [];
+let true_mail = prompt('Inserisci una email (try with ciao@123.it');
+let false_mail = 'ciao@123.it'
+/* mail_list.push(false_mail) */
+mail_list.push(true_mail);
+if (true_mail == mail_list && false_mail != mail_list) {
+    console.log('User has Joined')
+    mail_list.push(false_mail)
+    console.log(mail_list)
+} else {
+    console.log('Access denied')
+}
+
